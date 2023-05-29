830a84345f312ac3b07947ae44ef6da09b42d2c8
Tim <herew26@gmail.com>
2023-05-29 04:33:55 +0300

    feat: add app

---
+++ src/main/java/timnekk/orderflow/OrderFlowApplication.java
@@ -0,0 +1,15 @@
+package timnekk.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class OrderFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFlowApplication.class, args);
+    }
+
+}
